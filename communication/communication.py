6e5812ea1301e8a1dfb5234de4951f45132f9a41
Camila Franco de Sousa <imcamilafranco@gmail.com>
2019-07-03 15:12:47 +0000

    Update communication.py
    -- added interrupt and gps functions

--- communication/communication.py
+++ communication/communication.py
@@ -1,23 +1,43 @@
+def interruption_to_esp():
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
-
-
-def serial_communication():
     import serial
     port = "/dev/ttyUSB0"
     rate = 9600
     s1 = serial.Serial(port, rate)
-    s1.flushInput()
-    return
 
-
-def interrupt_to_esp():
-    serial_communication()
-    comp_list = ["Interrupt\r\n"]
+#TODO: recieve location function
     while True:
-        if s1.inWaiting() > 0:
-            inputValue = s1.readline()
-            print(inputValue)
-            if inputValue in comp_list:
-                s1.write('0')
-                return
+        if s1.inWaiting()>0:
+            #if inputValue in comp_list: RECIEVE_LOCATION = LOCATION
+            s1.write('0')
+            return;
+
+
+def gps_data():
+#! /usr/bin/python
+ 
+    from gps import *
+    import time
+    
+    gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE) 
+    print 'latitude\tlongitude\tspeed' # '\t' = TAB to try and output the data in columns.
+   
+    try:
+ 
+ 
+        while True:
+            report = gpsd.next() #
+            if report['class'] == 'TPV':
+             
+                print  getattr(report,'lat',0.0),"\t",
+                print  getattr(report,'lon',0.0),"\t",
+                #print  getattr(report,'epv','nan'),"\t",
+                #print  getattr(report,'ept','nan'),"\t",
+                print  getattr(report,'speed','nan'),"\t"
+                time.sleep(1) 
+ 
+    except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+        print "Done.\nExiting."
+        return;
+
