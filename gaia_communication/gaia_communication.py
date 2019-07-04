b6ea5d0aab28e6b48dc5f88e2be8f9849f271c07
Arthur Assis <arthur120496@gmail.com>
2019-07-04 12:40:46 -0400

    Updates pipy version

--- gaia_communication/gaia_communication.py
+++ gaia_communication/gaia_communication.py
@@ -24,11 +24,11 @@ def gps_data():
 
     gpsd = gps(mode=WATCH_ENABLE | WATCH_NEWSTYLE)
     # '\t' = TAB to try and output the data in columns.
-    
+
     lat = 0
     lon = 0
-    
-    while(lat == 0 && lon ==0):
+
+    while(lat == 0 and lon == 0):
         report = gpsd.next()
         if report['class'] == 'TPV':
 
@@ -37,5 +37,5 @@ def gps_data():
             # print(getattr(report,'epv','nan'),"\t")
             # print(getattr(report,'ept','nan'),"\t",)
             speed = getattr(report, 'speed', 'nan')
-    
+
     return (lat, lon, speed)
