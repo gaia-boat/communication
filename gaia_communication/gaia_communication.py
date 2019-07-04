c570890a46559546e221c68ef13e2d4021d58c37
Arthur Assis <arthur120496@gmail.com>
2019-07-03 21:11:57 -0400

    fixes stylesheet

--- gaia_communication/gaia_communication.py
+++ gaia_communication/gaia_communication.py
@@ -1,43 +1,42 @@
 def interruption_to_esp():
-#!/usr/bin/env/python
-# -*- coding: utf-8 -*-
+    # !/usr/bin/env/python
+    # -*- coding: utf-8 -*-
     import serial
     port = "/dev/ttyUSB0"
     rate = 9600
     s1 = serial.Serial(port, rate)
 
-#TODO: recieve location function
+# TODO: recieve location function
     while True:
-        if s1.inWaiting()>0:
-            #if inputValue in comp_list: RECIEVE_LOCATION = LOCATION
+        if s1.inWaiting() > 0:
+            # if inputValue in comp_list: RECIEVE_LOCATION = LOCATION
             s1.write('0')
-            return;
+            return
 
 
 def gps_data():
-#! /usr/bin/python
- 
+    # ! /usr/bin/python
+
     from gps import *
     import time
-    
-    gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE) 
-    print 'latitude\tlongitude\tspeed' # '\t' = TAB to try and output the data in columns.
-   
-    try:
- 
- 
-        while True:
-            report = gpsd.next() #
-            if report['class'] == 'TPV':
-             
-                print  getattr(report,'lat',0.0),"\t",
-                print  getattr(report,'lon',0.0),"\t",
-                #print  getattr(report,'epv','nan'),"\t",
-                #print  getattr(report,'ept','nan'),"\t",
-                print  getattr(report,'speed','nan'),"\t"
-                time.sleep(1) 
- 
-    except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-        print "Done.\nExiting."
-        return;
 
+    gpsd = gps(mode=WATCH_ENABLE | WATCH_NEWSTYLE)
+    # '\t' = TAB to try and output the data in columns.
+    print 'latitude\tlongitude\tspeed'
+
+    try:
+
+        while True:
+            report = gpsd.next()
+            if report['class'] == 'TPV':
+
+                print getattr(report, 'lat', 0.0), "\t",
+                print getattr(report, 'lon', 0.0), "\t",
+                # print  getattr(report,'epv','nan'),"\t",
+                # print  getattr(report,'ept','nan'),"\t",
+                print getattr(report, 'speed', 'nan'), "\t"
+                time.sleep(1)
+
+    except (KeyboardInterrupt, SystemExit):  # when you press ctrl+c
+        print "Done.\nExiting."
+        return
